3d6b96d756bb1eb9f281855aafd37eb624e5a663
Chris Malcolm <chrisishere@gmail.com>
2018-09-10 19:12:14 -0700

    [WIP] successfully serving messages

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -6,11 +6,15 @@ module.exports = {
     get: function (req, res) { // a function which handles a get request for all messages
       models.messages.get().then((results) => {
         console.log('results received: ', results);
+        debugger;
+        res.writeHead(200, {
+          'Content-Type': 'application/json'
+        });
+        res.end(JSON.stringify(results));
       }).catch((err) => {
         console.log('err occurred when fetching messages: ', err);
       });
-      // debugger;
-    }, 
+    },
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
